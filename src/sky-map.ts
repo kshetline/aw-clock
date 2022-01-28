a9a07e56e6d4355ad39fceaac7653c2aefdf0235
Kerry Shetline <kerry@shetline.com>
2022-01-28 15:48:43 -0500

    Add planet drawing.

--- src/sky-map.ts
+++ src/sky-map.ts
@@ -1,19 +1,15 @@
 import { AppService } from './app.service';
 import {
-  AsteroidCometInfo,
-  getSkyColor,
-  ISkyObserver,
-  NUTATION,
-  REFRACTION,
-  SkyObserver,
-  SolarSystem,
-  StarCatalog,
-  SUN
+  AsteroidCometInfo, Ecliptic, getSkyColor, ISkyObserver, JUPITER, MARS, MERCURY, MOON, NUTATION, REFRACTION, SATURN,
+  SkyObserver, SolarSystem, StarCatalog, SUN, TOPOCENTRIC, VENUS
 } from '@tubular/astronomy';
 import { getBinary } from './awc-util';
 import ttime from '@tubular/time';
 import julianDay = ttime.julianDay;
-import { atan2_deg, cos_deg, floor, max, min, mod, pow, round, sin_deg, SphericalPosition, sqrt, TWO_PI, Unit } from '@tubular/math';
+import {
+  abs, Angle, atan2_deg, cos_deg, floor, max, min, mod, mod2, Point, pow, round, sin_deg, SphericalPosition,
+  SphericalPosition3D, sqrt, TWO_PI, Unit
+} from '@tubular/math';
 import { colorFromRGB } from '@tubular/util';
 
 interface DrawingContext {
@@ -23,6 +19,7 @@ interface DrawingContext {
   xctr: number;
   yctr: number;
   pixelsPerArcSec: number;
+  planetFlags?: number;
   minStarBrightness?: number;
   scaleBoost?: number;
   starBrightestLevel?: number;
@@ -31,6 +28,11 @@ interface DrawingContext {
   skyObserver: ISkyObserver;
   jdu: number;
   jde: number;
+}
+
+export interface SortablePlanet {
+  planet: number;
+  pos: SphericalPosition3D;
 }
 
 const opacitiesOfWhite: string[] = [];
@@ -46,10 +48,24 @@ const BRIGHTEST_1x1_STAR_IMAGE_INDEX        = 500;
 const BRIGHTEST_3x3_STAR_IMAGE_INDEX        = 1500;
 const DEFAULT_SKY_RESOLUTION = 5;
 
+const planetColors = [
+  'yellow',  '#C0C0C0', '#EEEEFF', // Sun, Mercury, Venus
+  '#00CCCC', 'red',     'orange',  // Earth, Mars, Jupiter
+  'yellow', '', '', '', '#EEEEFF'  // Saturn, Moon
+];
+
+// These color specifications are left incomplete so that the alpha value can be varied.
+const SHADED_MOON            = 'rgba(102,153,204,';
+const INTERMEDIATE_MOON      = 'rgba(178,204,229,';
+const ILLUMINATED_MOON       = 'rgba(255,255,255,';
+
+const planetsToDraw = [SUN, MERCURY, VENUS, MARS, JUPITER, SATURN];
+
 export class SkyMap {
   private static starData: ArrayBuffer;
   private static starDataPromise: Promise<ArrayBuffer>;
 
+  private ecliptic = new Ecliptic();
   private facing = 0;
   private firstMag5 = 0;
   private minAlt = -0.00833;
@@ -77,7 +93,7 @@ export class SkyMap {
       getCometData(): Promise<AsteroidCometInfo[]> { return Promise.resolve(null); },
       getGrsData(): Promise<ArrayBuffer> { return Promise.resolve(null); },
     }, initialized => {
-      if (initialized)
+      if (initialized) // noinspection CommaExpressionJS
         this.starCatalog.forEach((star, index) => star.vmag <= 5 ? (this.firstMag5 = index, false) : true);
       else
         this.starCatalog = undefined;
@@ -106,7 +122,8 @@ export class SkyMap {
       yctr: round(height / 2),
       skyObserver: new SkyObserver(longitude, latitude),
       jdu,
-      jde: ttime.utToTdt(jdu)
+      jde: ttime.utToTdt(jdu),
+      planetFlags: NUTATION | REFRACTION
     } as DrawingContext;
     console.log(parseFloat(canvas.style.width), parseFloat(canvas.style.width) * 0.95, dc.pixelsPerArcSec);
     dc.scaleBoost = pow(dc.pixelsPerArcSec * 1.5 / SCALE_WHERE_BRIGHTEST_STAR_IS_3x3, 0.521);
@@ -117,11 +134,12 @@ export class SkyMap {
 
     dc.context.setTransform(canvasScaling, 0, 0, canvasScaling, 0, 0);
     this.drawSky(dc);
+    this.drawPlanets(dc);
     this.drawStars(dc);
   }
 
   private drawSky(dc: DrawingContext): void {
-    const sunPos = this.solarSystem.getHorizontalPosition(SUN, dc.jdu, dc.skyObserver, NUTATION | REFRACTION);
+    const sunPos = this.solarSystem.getHorizontalPosition(SUN, dc.jdu, dc.skyObserver, dc.planetFlags);
     const alt = sunPos.altitude.degrees;
     const totality = this.solarSystem.getLocalSolarEclipseTotality(dc.jde, dc.skyObserver);
     let skyColor: string;
@@ -179,20 +197,14 @@ export class SkyMap {
 
       const star = this.starCatalog.getStarInfo(i);
       const pos = this.starCatalog.getHorizontalPosition(i, dc.jdu, dc.skyObserver, 365.25, REFRACTION);
-      const alt = pos.altitude.degrees;
+      const pt = this.sphericalToScreenXY(pos, dc);
 
-      if (alt >= 0) {
-        const az = pos.azimuth.degrees;
-        const r = (90.0 - alt) * dc.size / 180.0 * 0.995;
-        const x = dc.xctr + cos_deg(-az + this.facing + 90.0) * r;
-        const y = dc.yctr + sin_deg(-az + this.facing + 90.0) * r;
-
-        this.drawStar(x, y, star.vmag, dc);
-      }
+      if (pt)
+        SkyMap.drawStar(pt.x, pt.y, star.vmag, dc);
     }
   }
 
-  private drawStar(x: number, y: number, vmag: number, dc: DrawingContext, colorForPlanetDrawnAsStar?: string): void {
+  private static drawStar(x: number, y: number, vmag: number, dc: DrawingContext, colorForPlanetDrawnAsStar?: string): void {
     const maxRange = (colorForPlanetDrawnAsStar ? 2000 - dc.starDimmestLevel - 1 : dc.starLevelRange);
     const brightness = min(max(dc.starLevelRange - round((vmag + 1.0) / 7.0 *
           dc.starLevelRange * 1.2), 0), maxRange) + dc.starDimmestLevel;
@@ -212,6 +224,134 @@ export class SkyMap {
     }
   }
 
+  private drawPlanets(dc: DrawingContext): void {
+    let planets: SortablePlanet[] = [];
+
+    planetsToDraw.forEach(p => {
+      planets.push({ planet: p, pos: this.getSphericalPosition(p, dc) as SphericalPosition3D });
+    });
+
+    planets = planets.sort((a, b) => b.pos.radius - a.pos.radius);
+
+    for (const planet of planets) {
+      const p = planet.planet;
+      const pt = this.sphericalToScreenXY(planet.pos, dc);
+
+      if (pt)
+        this.drawPlanet(p, pt, dc);
+
+      // if (pt || p === this.specialLabelIndex)
+      //   this.qualifyBodyForSelection(pt, SELECTION_TYPE.PLANET, p, Boolean(pt), dc);
+    }
+  }
+
+  private drawPlanet(planet: number, pt: Point, dc: DrawingContext): void {
+    const { x, y } = pt;
+    let size = 3;
+    let color = planetColors[planet];
+
+    if (planet === SUN || planet === MOON) {
+      if (dc.pixelsPerArcSec > 0.0) {
+        size = round(this.solarSystem.getAngularDiameter(planet, dc.jde) * dc.pixelsPerArcSec);
+        size += (size + 1) % 2;
+      }
+
+      if (size < 6)
+        size = 6;
+    }
+
+    const r0 = floor(size / 2);
+
+    if (planet === MOON) {
+      const phase = this.solarSystem.getLunarPhase(dc.jde);
+      const coverage = (cos_deg(phase) + 1.0) / 2.0;
+      const shadeAngle = this.getMoonShadingOrientation(dc);
+      const sin_sa = sin_deg(shadeAngle);
+      const cos_sa = cos_deg(shadeAngle);
+      const r02 = r0 * r0;
+
+      for (let dy = -r0 - 1; dy <= r0 + 1; ++dy) {
+        for (let dx = -r0 - 1; dx <= r0 + 1; ++dx) {
+          const rot_x = dx * cos_sa + dy * sin_sa;
+          const rot_y = dy * cos_sa - dx * sin_sa;
+          const r = sqrt(rot_x * rot_x + rot_y * rot_y);
+
+          if (r <= r0 + 1) {
+            let alpha = 1.0;
+
+            if (r > r0)
+              alpha = 1.0 - r + r0;
+
+            if (abs(mod2(phase, 360)) < 20.0) {
+              color = SHADED_MOON;
+            }
+            else if (abs(phase - 180.0) < 20.0)
+              color = ILLUMINATED_MOON;
+            else {
+              const lineWidth = 2 * sqrt(max(r02 - rot_y * rot_y, 0.0)) + 1.0;
+              const inset = rot_x + (lineWidth - 1.0) / 2;
+              const shadowWidth = coverage * lineWidth;
+              let leftSpan: number;
+
+              if (phase <= 180.0)
+                leftSpan = shadowWidth - 0.5;
+              else
+                leftSpan = lineWidth - shadowWidth - 0.5;
+
+              if ((phase <= 180.0 && inset < leftSpan + 0.25) ||
+                  (phase  > 180.0 && inset > leftSpan + 0.25))
+                color = SHADED_MOON;
+              else if (abs(inset - leftSpan) <= 0.5) {
+                color = INTERMEDIATE_MOON;
+              }
+              else
+                color = ILLUMINATED_MOON;
+            }
+
+            dc.context.fillStyle = color + alpha + ')';
+            dc.context.fillRect(x + dx, y + dy, 1, 1);
+          }
+        }
+      }
+    }
+    else {
+      // If scaled drawing is being done, draw potentially bright planets as stars before drawing them in the
+      // usual rectangle/circle form, just in case the star form would be larger, so that a bright planet
+      // doesn't get lost by size comparison amid otherwise larger/brighter looking stars.
+      if (dc.pixelsPerArcSec > 0.0 && planet === VENUS) {
+        const vmag = this.solarSystem.getMagnitude(planet, dc.jde);
+
+        SkyMap.drawStar(pt.x, pt.y, vmag, dc, color);
+      }
+
+      dc.context.fillStyle = color;
+
+      if (size <= 3)
+        dc.context.fillRect(x - r0, y - r0, size, size);
+      else {
+        dc.context.beginPath();
+        dc.context.arc(x + 0.5, y + 0.5, r0, 0, TWO_PI);
+        dc.context.fill();
+      }
+    }
+  }
+
+  private sphericalToScreenXY(pos: SphericalPosition, dc: DrawingContext): Point {
+    return this.horizontalToScreenXY(pos.altitude.degrees, pos.azimuth.degrees, dc);
+  }
+
+  private horizontalToScreenXY(alt: number, az: number, dc: DrawingContext): Point {
+    if (alt >= 0) {
+      const r = (90.0 - alt) * dc.size / 180.0 * 0.995;
+      const x = dc.xctr + cos_deg(-az + this.facing + 90.0) * r;
+      const y = dc.yctr + sin_deg(-az + this.facing + 90.0) * r;
+
+      return { x, y };
+    }
+
+    return null;
+  }
+
   private screenXYToHorizontal(x: number, y: number, dc: DrawingContext): SphericalPosition {
     const dx = x - dc.xctr;
     const dy = y - dc.yctr;
@@ -220,5 +360,39 @@ export class SkyMap {
     const alt = 90.0 - r / dc.size * 180.0;
 
     return new SphericalPosition(az, alt, Unit.DEGREES, Unit.DEGREES);
+  }
+
+  private getMoonShadingOrientation(dc: DrawingContext): number {
+    let dt = 0.0;
+    let moonPara: Angle;
+
+    // In the very unlikely event that the moon is precisely at the zenith, which makes the
+    // parallactic angle calculation fail, we'll just move on to a slightly later moment in
+    // time to fix the problem.
+    //
+    while (!(moonPara = this.solarSystem.getParallacticAngle(MOON, dc.jdu + dt, dc.skyObserver)))
+      dt += 1.0E-5;
+
+    const eclipticPos = this.solarSystem.getEclipticPosition(MOON, dc.jde + dt, dc.skyObserver);
+    const obliquity = this.ecliptic.getNutation(dc.jde + dt).ε;
+    const horizontalPos = this.getSphericalPosition(MOON, dc);
+
+    moonPara = moonPara.add(obliquity.multiply(eclipticPos.longitude.cos));
+
+    let angle = moonPara.degrees;
+    const az = horizontalPos.azimuth.degrees;
+
+    angle += this.facing - az;
+
+    return angle;
+  }
+
+  private getSphericalPosition(bodyIndex: number, dc: DrawingContext): SphericalPosition {
+    let flags = dc.planetFlags;
+
+    if (bodyIndex === MOON)
+      flags |= TOPOCENTRIC;
+
+    return this.solarSystem.getHorizontalPosition(bodyIndex, dc.jdu, dc.skyObserver, flags);
   }
 }
